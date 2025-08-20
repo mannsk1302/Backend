fcc480502d9233af4257339a81867827d406e200
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-08-21 00:45:38 +0530

    Set up the React frontend project for Chapter 02 with Vite, configured ESLint, added styling files, and updated `.gitignore`.

--- Chapter_02/Backend/server.js
+++ Chapter_02/Backend/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('Server is running!')
 });
 
-app.get('/jokes', (req, res) => {
+app.get('/api/jokes', (req, res) => {
     const jokes = [
         {
             id: 1,
